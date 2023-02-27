30aec3c3ef8f4bc54fa83c431d8c391abe2106ae
pridhvi <pridhvimuthuraju@gmail.com>
2023-02-27 13:01:37 -0500

    Added HashRouter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AboutComponent from './about';
 import { Routes, Route } from 'react-router'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import NavigationBar from './navigation-bar';
 import ProjectsComponent from './projects';
 import ContactComponent from './contact';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="row m-0 mt-4 mt-sm-5">
         <NavigationBar />
         <Routes>
@@ -20,7 +20,7 @@ function App() {
         {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link> */}
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/about/index.js
+++ src/about/index.js
@@ -2,8 +2,35 @@ import React from 'react'
 
 const AboutComponent = () => {
   return (
-    <div className="container col-12 col-sm-10 col-md-9 bg-white">
-        AboutComponent
+    <div className="container d-flex justify-content-center">
+      <div className='col-12 col-sm-10 col-md-9 bg-gray row p-4 wd-rounded-border wd-text-gray'>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+        </span>
+      </div>
     </div>
   )
 }
