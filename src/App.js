4391b3648e7a3cb47ed9c1133a4a4a50ec2c737b
pridhvi <pridhvimuthuraju@gmail.com>
2023-02-28 00:50:53 -0500

    fixed image warping

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ function App() {
           <Route path="/education" element={<EducationComponent />} />
           <Route path="/contact" element={<ContactComponent />} />
         </Routes>
-        {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link> */}
       </div>
     </HashRouter>
   );
--- src/about/index.js
+++ src/about/index.js
@@ -5,10 +5,10 @@ import './index.css'
 const AboutComponent = () => {
   return (
     <div className="container">
-      <div className='row d-flex justify-content-center'>
-        <div className='col-8 col-md-6 col-lg-5 col-xl-4 col-xxl-3 bg-gray p-0 mb-3 mb-md-5 wd-rounded-border wd-text-gray shadow-lg'>
-          <img className='wd-rounded-border'
-            src='./images/pridhvi-dp.jpeg' width='100%' height='100%' alt='profile dp' />
+      <div className='row d-flex justify-content-center align-items-center'>
+        <div className='col-8 col-md-6 col-lg-5 col-xl-4 col-xxl-3 p-0 mb-3 mb-md-5 wd-rounded-border'>
+          <img className='wd-rounded-border shadow-lg'
+            src='./images/pridhvi-dp.jpeg' width='100%' alt='profile dp' />
         </div>
 
         <div className='col-11 col-md-7 col-lg-6 col-xl-5 col-xxl-4 ms-lg-5'>
@@ -34,7 +34,7 @@ const AboutComponent = () => {
       </div>
 
       <div className='row d-flex justify-content-center'>
-        <div className='col-9 col-sm-7 col-md-6 col-lg-5 col-xl-4 col-xxl-3 mb-3 mb-md-5 bg-gray p-4 wd-rounded-border wd-text-gray shadow-lg d-flex justify-content-center'>
+        <div className='col-9 col-sm-7 col-md-6 col-lg-5 col-xl-4 col-xxl-3 bg-gray p-4 wd-rounded-border wd-text-gray shadow-lg d-flex justify-content-center'>
           <h4>
             Download my <a href='./files/pridhvi-resume.pdf' download className="btn btn-dark border-secondary rounded-pill shadow"><i className="bi bi-file-earmark-arrow-down"></i> Resume</a>
           </h4>
